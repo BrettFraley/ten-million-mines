69a13bc1b084c4c67e9a795d439da77605d492a2
BrettFraley <brettfraley@proton.me>
2024-08-06 15:57:02 -0400

    Adds rendering logic for dom, along with strategy for a selected substring chart in a span using getSelected -Next up is to complete that event logic in th dom, assignment of a field, then server and SSE updates.

--- mines.js
+++ mines.js
@@ -6,46 +6,110 @@ dom = {
         parent.appendChild(el)
     }
 }
-
+// generateField returns a 'field' element
+// containing  all rows and game 'cells'
 const generateField = config => {
 
-    const field = dom.getEl('field')
-
-    // TODO: Put cell count, and other config for testing
-    // and dev in config
-    const CELL_COUNT = 10000
-    const ROWS = 100
-    const XWIDTH = 100
-
-
-    const buildRowCells = (amt, rowId, row) => {
-        let cellElements = dom.createEl()
+    const buildRowCells = (amt) => {
+        let rowStr = ""
         for (let i = 0; i < amt; i++) {
-            const pre = dom.createEl('pre')
-            pre.className = "field-cell"
-            pre.innerText = "#"
-            pre.id = `${rowId}_${i}`
-            row.appendChild(pre)
+            rowStr += "#"
         }
+        return rowStr
     }
 
     const genRow = rowNum => {
         let row = dom.createEl('span')
         row.className = "field-row"
         row.id = `${rowNum}-mine-field-row`
-        dom.appendEl('field', row)
-        buildRowCells(XWIDTH, rowNum, row)
-        
+        row.innerText = buildRowCells(config.FIELD_ROW_CELL_COUNT)
+        dom.appendEl('field', row)      
     }
 
-    // generate 100 spans with innerText of 100 \w
     const buildField = fieldSize => {
         for (let i = 0; i < fieldSize; i++) {
             genRow(i)
         }
     }
 
-    buildField(100)
+    buildField(config.FIELD_ROW_COUNT)
+    return dom.getEl('field')
 }
 
-generateField()
+const CONFIG = {
+    FIELD_ROW_COUNT: 100,
+    FIELD_ROW_CELL_COUNT: 100
+}
+
+const mineGame = {
+    init: () => {
+        let field = generateField(CONFIG)
+        
+        field.addEventListener('click', () => {
+            const selection = document.getSelection()
+            // text in a row is highlighted    
+            if (!selection.isCollapsed) {
+
+                ancOff = selection.anchorOffset
+                focOff = selection.focusOffset
+
+                // single cell is highlighted
+                if (focOff - ancOff === 1) {
+                    let row = selection.baseNode.parentElement
+                    let rowId = row.attributes.id.value.split('-')[0]
+                    console.log(rowId)
+                    console.log(focOff)
+
+                    mineGame.updateFieldRowCell(rowId, focOff)
+                }
+
+                
+            }
+        }, false)
+
+    },
+
+    updateFieldRowCell: (rowId, idx) => {
+        const row = dom.getEl(`${rowId}-mine-field-row`)
+        let cur = row.innerText
+        console.log(typeof cur)
+        console.log(cur[idx])
+        row.innerText = cur
+        
+    }
+
+
+
+
+
+
+
+}
+
+mineGame.init()
+
+// getSelection
+// getSelection.baseNode.parentElement => 
+        // will reference the row span,
+        // which coontains the row ID
+
+        // 25th row
+        // selction.achorOffset is the start 
+        // selection.focusOffset is end
+
+// What if only cells within a row may be highlighted and cleared
+// And as a group rows get cleared and eliminated
+// Or something like only 2 or 3 may be cleared at a time
+// Maybe there's a delay before you may clear your next
+// cell or set of cells
+
+
+
+
+
+
+
+
+
+
+
